a938e9f73e25cf6ed9586444a0b863cdee845d01
Yangqinghua <13146500045@163.com>
2024-11-21 16:24:35 +0800

    <feat>add map and map SDK

--- frontend/pages/index/index.vue
+++ frontend/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="full-screen">
+		<map class="map" latitude='23.099' longitude='113.325'  scale='12' 
+		controls='{{con}}' markers='{{mar}}' bindcontroltap='con' bindmarkertap='mar'
+		enable-rotate="true">
+		</map>
 	</view>
 </template>
 
@@ -24,29 +24,16 @@
 </script>
 
 <style>
-	.content {
+	.full-screen {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.map{
+		height: 1300rpx;
+		width: 100%;
 	}
 </style>
+
