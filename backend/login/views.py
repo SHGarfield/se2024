75f4ede9b36f700fc0b03a2b0881039410d3ed43
Yangqinghua <13146500045@163.com>
2024-12-14 01:26:12 +0800

    <feat>重构后端，增加添加用户功能（首次登录将信息录入数据库）

--- backend/login/views.py
+++ backend/login/views.py
@@ -10,9 +10,10 @@ from login import models
 
 
 def wechat_login(request):
+    print("\n---[wechat_login]---")
     if request.method == "POST":
         # 从微信服务器获取openid
-        response=getOpenIdFromWechat(request)
+        response = getOpenIdFromWechat(request)
         response_data = json.loads(response.text)
         print("response_data: ", response_data)
 
@@ -47,13 +48,14 @@ def wechat_login(request):
     else:
         return JsonResponse({"status": "fail", "error": "仅支持POST请求"})
 
+
 def getOpenIdFromWechat(request):
     # 获得数据报
     data = json.loads(request.body)
-    print('request_body',data)
+    print("request_body", data)
     # 从主体内容中提取code
     code = data.get("code")
-    print('code',code)
+    print("code", code)
     appid = "wxb5dbc4b4be36808f"
     secret = "fff193612d5d2bea5e85689abe27e14a"
     url = f"https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={code}&grant_type=authorization_code"
@@ -66,16 +68,6 @@ def isUserOpenIdExist(_openid):
     return models.UserInfo.objects.filter(openid=_openid).exists()
 
 
-def add_user_to_db(response_data, data):
-    new_instance = models.UserInfo()
-    new_instance.openid = response_data["openid"]
-    new_instance.username = data.get("nickname")
-    new_instance.email = "未填写"
-    new_instance.gender = "未填写"
-    new_instance.save()
-    print(models.UserInfo.objects.all().first())
-
-
 # from django.http import JsonResponse
 # from django.views.decorators.csrf import csrf_exempt
 import os
@@ -86,6 +78,7 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # @csrf_exempt  # 如果在开发阶段跳过 CSRF 验证
 def upload_avatar(request):
+    print("\n---[upload_avatar]---")
     if request.method == "POST" and request.FILES.get("avatar"):
         avatar = request.FILES["avatar"]  # 获取上传的头像文件
         file_path = os.path.join(UPLOAD_DIR, avatar.name)
@@ -101,18 +94,55 @@ def upload_avatar(request):
         # 将文件路径保存到数据库
         user = models.UserInfo.objects.filter(openid=openid).first()
         user.avatar_url = file_path
-
-        all_entries = models.UserInfo.objects.all()
-        for entry in all_entries:
-            print(
-                "--user",
-                entry.openid,
-                entry.username,
-                entry.email,
-                entry.gender,
-                entry.avatar_url,
-            )
+        user.save()
+        showAllUser()
         return JsonResponse(
             {"code": 200, "message": "头像上传成功", "file_path": file_path}
         )
     return JsonResponse({"code": 400, "message": "请求无效"}, status=400)
+
+
+def upload_username(request):
+    print("\n---[upload_username]---")
+    if request.method == "POST":
+        # 获得数据报
+        data = json.loads(request.body)
+        print("request_body", data)
+        # 从主体内容中提取code
+        openid = data.get("openid")
+        nickname = data.get("nickname")
+        try:
+            add_user_to_db(openid, nickname)
+            return JsonResponse(
+                {"status": "success", "message": "Username updated successfully"}
+            )
+        except Exception as e:
+            # 其他错误处理
+            return JsonResponse({"status": "error", "message": str(e)}, status=500)
+    else:
+        # 如果不是POST请求，返回错误信息
+        return JsonResponse(
+            {"status": "error", "message": "Invalid request method"}, status=405
+        )
+
+
+def add_user_to_db(openid, username):
+    new_instance = models.UserInfo()
+    new_instance.openid =openid
+    new_instance.username = username
+    new_instance.email = "未填写"
+    new_instance.gender = "未填写"
+    new_instance.save()
+    
+def showAllUser():
+    print("\n---[showAllUser]---")
+    all_entries = models.UserInfo.objects.all()
+    for entry in all_entries:
+        print(
+            "--user",
+            entry.openid,
+            entry.username,
+            entry.email,
+            entry.gender,
+            entry.avatar_url,
+        )
