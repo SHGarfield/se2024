a7f8556fc6388c86f0e892a3d9fc2c36ff67ce5c
Yangqinghua <13146500045@163.com>
2024-12-24 08:55:55 +0800

    <feat>前端：增加修改帖子可见范围

--- frontend/pages/my_map_detail/my_map_detail.vue
+++ frontend/pages/my_map_detail/my_map_detail.vue
@@ -8,10 +8,12 @@
 					<text class="item-content">{{itemData.content}}</text>
 					<text class="modified-time">{{itemData.modified_time}}</text>
 				</view>
-
 			</view>
 		</scroll-view>
 		<view class="bottom-bar">
+			<picker mode="selector" :range="pickerRange" @change="onPickerChange">
+				<button class="change_isprivate" >修改可见范围</button>
+			</picker>
 			<button class="save-route-button" @click="editRoute">编辑路线</button>
 		</view>
 	</view>
@@ -30,13 +32,7 @@
 	} from '@dcloudio/uni-app';
 	// const isRefreshing = ref(false);
 	const itemData = ref({});
-	// const handlePlan = (markers) => {
-	// 	getApp().globalData.marks = markers;
-	// 	console.log(markers);
-	// 	wx.switchTab({
-	// 		url: '/pages/map/map' // 替换为实际的目标页面路径
-	// 	});
-	// }
+	const pickerRange=ref(["公开可见","仅自己可见"]);
 	const modified_time = ref("");
 	// const format_modified_time = () => {
 	// 	let time = getApp().globalData.modified_time;
@@ -59,6 +55,50 @@
 			url: '/pages/my_map_edit/my_map_edit'
 		})
 	};
+	const onPickerChange = (e) => {
+		const pickerValue = pickerRange.value[e.detail.value];
+		if (pickerValue == "公开可见") {
+			//公开可见
+			setRouteIsPrivate(false);
+			console.log(1);
+		} else {
+			//仅自己可见
+			setRouteIsPrivate(true);
+			console.log(2);
+		}
+	};
+	
+	const setRouteIsPrivate = (isprivate) => {
+		wx.request({
+			url: 'http://111.229.117.144:8000/dealMarks/setRouteIsPrivate/', // 后端API地址
+			method: 'PUT',
+			data: {
+				openid: getApp().globalData.openid,
+				routeid: itemData.value.id,
+				isprivate: isprivate
+			},
+			success: function(res) {
+				console.log('修改可见范围成功', res);
+				wx.showToast({
+					title: '修改成功', // 提示内容
+					icon: 'success', // 图标类型
+					duration: 1000 // 提示框停留时间
+				});
+				setTimeout(function() {
+					backToMap();
+				}, 2000);
+			},
+			fail: function(err) {
+				console.error('数据提交失败', err);
+				wx.showToast({
+					title: '修改失败', // 提示内容
+					icon: 'error', // 图标类型
+					duration: 1000 // 提示框停留时间
+				});
+			}
+		});
+	};
+	
 	const saveRoute = () => {
 		//检查是否登录
 		if (getApp().globalData.openid) {
@@ -148,6 +188,24 @@
 		padding: 10rpx;
 		height: 120rpx;
 		width: 98%;
+	}
+
+	.change_isprivate {
+		position: absolute;
+		top: 15rpx;
+		right: 300rpx;
+		background-color: white;
+		border: 1px solid #007AFF;
+		color: #007AFF;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		border-radius: 60rpx;
+		font-weight: 400;
+		font-size: 27rpx;
+		width: 250rpx;
+		height: 80rpx;
 	}
 
 	.save-route-button {
